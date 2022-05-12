a748854cf08f7a20f70607ede9a8776818b55eaa
lsm <seungmin941215@awasoft.co.kr>
2022-05-12 16:54:45 +0900

    조건부 렌더링

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -4,17 +4,10 @@ import Wrapper from './Wrapper'
 import './App.css'
 
 function App() {
-    const name = 'react'
-    const style = {
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: 24, // 기본 단위 px
-        padding: '1rem', // 다른 단위 사용 시 문자열로 설정
-    }
-
     return (
         <Wrapper>
-            <Hello name="react" color="red" />
+            <Hello name="react" color="red" isSpecial />
+            {/* isSpecial={true} === isSpecial */}
             <Hello color="pink" />
         </Wrapper>
     )
