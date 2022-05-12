74038901d28eba7a74ea25f54b3ba40af6581676
lsm <seungmin941215@awasoft.co.kr>
2022-05-12 16:12:49 +0900

    esLint, prettier 설정

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -4,20 +4,20 @@ import Wrapper from './Wrapper'
 import './App.css'
 
 function App() {
-  const name = 'react'
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24, // 기본 단위 px
-    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
-  }
+    const name = 'react'
+    const style = {
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: 24, // 기본 단위 px
+        padding: '1rem', // 다른 단위 사용 시 문자열로 설정
+    }
 
-  return (
-    <Wrapper>
-      <Hello name="react" color="red"/>
-      <Hello color="pink" />
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <Hello name="react" color="red" />
+            <Hello color="pink" />
+        </Wrapper>
+    )
 }
 
-export default App;
+export default App
