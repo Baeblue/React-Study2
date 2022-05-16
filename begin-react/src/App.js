6b12055feac83d13dbb80b190da6eb0ba101a737
lsm <seungmin941215@awasoft.co.kr>
2022-05-17 08:38:50 +0900

    input 상태 관리하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hello from './Hello'
 import Wrapper from './Wrapper'
 import Counter from './Counter'
+import InputSample from './InputSample'
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
                 <Hello color="pink" />
             </Wrapper>
             <Counter />
+            <InputSample />
         </div>
     )
 }
