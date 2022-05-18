fbdd0bcf26dc1de0944821190486a0a0f21f4d14
lsm <seungmin941215@awasoft.co.kr>
2022-05-19 08:41:51 +0900

    여러 개의 input 상태 관리하기_의문점 있음

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,22 +1,36 @@
 import React, { useState } from 'react'
 
 function InputSample() {
-    const [text, setText] = useState('')
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: '',
+    })
+
+    const { name, nickname } = inputs
 
     const onChange = (e) => {
-        setText(e.target.value)
+        const { value, name } = e.target
+        setInputs({
+            ...inputs, // 이 부분의 이해가 어렵다. inputs 안에 있는 name과 nickname을 일단 가져와서
+            [name]: value, // 바꿀 부분만 바꿔주는 건가? 위에서 가져온 두 값 중 하나는 무조건 갱신되는건가?
+        })
     }
 
     const onReset = () => {
-        setText('')
+        setInputs({
+            name: '',
+            nickname: '',
+        })
     }
 
     return (
         <div>
-            <input onChange={onChange} value={text} />
+            <input placeholder="이름" name="name" onChange={onChange} value={name} />
+            <input placeholder="닉네임" name="nickname" onChange={onChange} value={nickname} />
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값: {text}</b>
+                <b>값: </b>
+                {name} ({nickname})
             </div>
         </div>
     )
