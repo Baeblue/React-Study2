293a3282e2d13d6fba8286d9c0229d1f2f07d3f2
lsm <seungmin941215@awasoft.co.kr>
2022-05-24 13:45:11 +0900

    배열 내 항목 수정하기_click 이벤트에 따른 스타일 수정

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -20,16 +20,19 @@ function App() {
             id: 1,
             username: '짱구',
             email: '짱구@example.com',
+            active: true,
         },
         {
             id: 2,
             username: '곰돌이푸',
             email: '곰돌이푸@example.com',
+            active: false,
         },
         {
             id: 3,
             username: '잔망루피',
             email: '잔망루피@example.com',
+            active: false,
         },
     ])
 
@@ -57,10 +60,14 @@ function App() {
         setUsers(users.filter((user) => user.id !== id))
     }
 
+    const onToggle = (id) => {
+        setUsers(users.map((user) => (user.id === id ? { ...user, active: !user.active } : user)))
+    }
+
     return (
         <>
             <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-            <UserList users={users} onRemove={onRemove} />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
         </>
     )
 }
--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,22 +1,25 @@
 import React from 'react'
 
-function User({ user, onRemove }) {
+function User({ user, onRemove, onToggle }) {
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            <b onClick={() => onToggle(user.id)} style={{ cursor: 'pointer', color: user.active ? 'green' : 'black' }}>
+                {user.username}
+            </b>{' '}
+            <span>({user.email})</span>
             <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     )
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
             {users.map(
                 (
                     user, // (user, index) index를 key로 사용해도 무방
                 ) => (
-                    <User user={user} key={user.id} onRemove={onRemove} />
+                    <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />
                 ),
             )}
         </div>
